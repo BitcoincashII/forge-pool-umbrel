2a4b96b5782dd81097eaebdcf6023660ce004a4c
Bitcoin Cash II <dev@bch2.org>
2026-03-22 19:11:30 +0000

    Fix: Remove duplicate PoolConfig declaration

--- cmd/stratum/main.go
+++ cmd/stratum/main.go
@@ -490,11 +490,9 @@ func main() {
 
 	// Check for user-configured port from pool config file
 	stratumPort := config.GetInt("stratum.port")
-	if dataDir := os.Getenv("DATA_DIR"); dataDir != "" {
-		if poolCfg, err := loadPoolConfig(dataDir); err == nil && poolCfg.StratumPort > 0 {
-			stratumPort = poolCfg.StratumPort
-			logger.Info("Using stratum port from user config", zap.Int("port", stratumPort))
-		}
+	if poolCfg := loadPoolConfigFromJSON(); poolCfg != nil && poolCfg.StratumPort > 0 {
+		stratumPort = poolCfg.StratumPort
+		logger.Info("Using stratum port from user config", zap.Int("port", stratumPort))
 	}
 
 	serverConfig := &stratum.ServerConfig{
@@ -879,24 +877,6 @@ func main() {
 		stratumBraiinsServer.Stop()
 	}
 	stratumServer.Stop()
-}
-
-// PoolConfig for reading user settings
-type PoolConfig struct {
-	StratumPort int `json:"stratum_port"`
-}
-
-func loadPoolConfig(dataDir string) (*PoolConfig, error) {
-	configPath := dataDir + "/config/pool-config.json"
-	data, err := os.ReadFile(configPath)
-	if err != nil {
-		return nil, err
-	}
-	var cfg PoolConfig
-	if err := json.Unmarshal(data, &cfg); err != nil {
-		return nil, err
-	}
-	return &cfg, nil
 }
 
 func loadConfig(path string) (*viper.Viper, error) {
